9f54ea2afc296d4cf3e7f37b0b4b8a4b0496459a
Priyankabolem <bolempriyanka10@gmail.com>
2023-09-12 17:04:58 -0500

    updated swagatam

--- FirstApp/Priyankabolem/PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
+++ FirstApp/Priyankabolem/PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlayWithAlphaNumVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.headerLBL.text = String(format:"%@\nPlay with AlphaNumberics","\u{0c38}\u{0c4d}\u{0c35}\u{0c3e}\u{0c17}\u{0c24}\u{0c02}")
     }
 
-
+    @IBOutlet weak var headerLBL: UILabel!
+    
 }
 
