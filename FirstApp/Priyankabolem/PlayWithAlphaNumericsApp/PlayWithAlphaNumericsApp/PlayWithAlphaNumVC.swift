3234bd20514c6f81068981dd5ee22f3811cf78ee
Priyankabolem <bolempriyanka10@gmail.com>
2023-09-12 20:04:21 -0500

    Outlets and actions

--- FirstApp/Priyankabolem/PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
+++ FirstApp/Priyankabolem/PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class PlayWithAlphaNumVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.headerLBL.text = String(format:"%@\nPlay with AlphaNumberics","\u{0c38}\u{0c4d}\u{0c35}\u{0c3e}\u{0c17}\u{0c24}\u{0c02}")
     }
-
+    
     @IBOutlet weak var headerLBL: UILabel!
     
     @IBAction func secondNumStepper(_ sender: Any) {
@@ -23,5 +23,35 @@ class PlayWithAlphaNumVC: UIViewController {
     }
     @IBOutlet weak var secondNumLBL: UILabel!
     @IBOutlet weak var firstNumLBL: UILabel!
+    
+    @IBAction func firstStrTF(_ sender: Any) {
+    }
+    
+    @IBAction func numberSWCH(_ sender: Any) {
+    }
+    
+    @IBAction func secondStrTF(_ sender: Any) {
+    }
+    
+    @IBAction func optionsSWCH(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func generatePattern(_ sender: UIButton) {
+    }
+    
+    @IBAction func manipulateStrings(_ sender: UIButton) {
+    }
+    
+    
+    @IBOutlet var stringAndPatternBTN: [UIButton]!
+    
+    @IBOutlet var numbersStepper: [UIStepper]!
+    
+    @IBOutlet weak var messageTV: UITextView!
+    
+    
+   
+    
+    
 }
-
